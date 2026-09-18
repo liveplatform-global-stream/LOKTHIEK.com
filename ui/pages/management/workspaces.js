bb5be9c951875c029b05d01a1e65b38a8f9cd73b
https://lokthiek.com <199331039+Lok-thiek@users.noreply.github.com>
2026-09-18 12:53:46 +0100

    Initialize leecalcote/docs/managing-aws-infra

---
+++ ui/pages/management/workspaces.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NoSsr } from '@mui/material';
+import { connect } from 'react-redux';
+import Head from 'next/head';
+import { WorkspacesComponent } from '../../components/Lifecycle';
+import { Box } from '@layer5/sistent';
+
+const Workspaces = () => {
+  return (
+    <NoSsr>
+      <Head>
+        <title>Workspaces | Meshery</title>
+      </Head>
+      <Box sx={{ maxWidth: '90%', margin: 'auto', overflow: 'hidden' }}>
+        <WorkspacesComponent />
+      </Box>
+    </NoSsr>
+  );
+};
+
+export default connect(null)(Workspaces);
