b516ccae638c87c355660907d19bb6369a896c17
https://lokthiek.com <199331039+Lok-thiek@users.noreply.github.com>
2026-09-18 12:45:01 +0100

    Initialize addIsNamespace

---
+++ docs/_models/flux.md
@@ -0,0 +1,24 @@
+---
+layout: integration
+title: Flux
+subtitle: Collaborative and visual infrastructure as design for Flux
+image: /assets/img/integrations/flux/icons/color/flux-color.svg
+permalink: extensibility/integrations/flux
+docURL: https://docs.meshery.io/extensibility/integrations/flux
+description: 
+integrations-category: App Definition and Development
+integrations-subcategory: Continuous Integration & Delivery
+registrant: Artifact Hub
+components: 
+featureList: [
+  "Synchronizes Kubernetes state with Git",
+  "Continuous reconciliation",
+  "Supports various Git providers"
+]
+howItWorks: "Integrates Flux configurations"
+howItWorksDetails: "Streamlined GitOps continuous delivery in Kubernetes"
+language: en
+list: include
+type: extensibility
+category: integrations
+---
---
+++ server/models/pattern/resource/selector/workload.go
@@ -0,0 +1,49 @@
+package selector
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/meshery/schemas/models/v1beta1/component"
+
+	regv1beta1 "github.com/layer5io/meshkit/models/meshmodel/registry/v1beta1"
+)
+
+func (s *Selector) GetDefinition(name string, version string, modelName string, apiVersion string) (component.ComponentDefinition, error) {
+	var comp *component.ComponentDefinition
+	name = strings.Split(name, ".")[0]
+	fmt.Println(name, modelName, version, apiVersion)
+	if modelName == ""  {
+		return component.ComponentDefinition{}, fmt.Errorf("model name is required")
+	}
+	
+	if apiVersion == "" {
+		return component.ComponentDefinition{}, fmt.Errorf("apiVersion is required")
+	}
+
+	entities, _, _, _ := s.registry.GetEntities(&regv1beta1.ComponentFilter{
+		Name:       name,
+		ModelName:  modelName,
+		APIVersion: apiVersion,
+	})
+	
+	found := false
+	for _, en := range entities {
+		if en != nil {
+			var ok bool
+			comp, ok = en.(*component.ComponentDefinition)
+			if ok {
+				found = true
+			}
+			if comp.Model.Version == version { //prefer to use the correct version, if available
+				break
+			}
+		}
+	}
+	if !found || comp == nil {
+		component := component.ComponentDefinition{}
+		return component, fmt.Errorf(fmt.Sprintf("could not find component with name: %s, model: %s, apiVersion: %s", name, modelName, apiVersion))
+	}
+	return *comp, nil
+}
+
